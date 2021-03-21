2a4b2f7f1f825298c9a58ccf21063874c4b57286
Charles L <charles.lobo@gmail.com>
2021-03-22 09:42:14 +1100

    feat: rename interface to Everlife Explorer

--- index.js
+++ index.js
@@ -20,14 +20,14 @@ let ssbConfig = null
 let quitting = false
 
 /**
- * It's not possible to run two instances of patchwork as it would create two
- * ssb-server instances that conflict on the same port. Before opening patchwork,
+ * It's not possible to run two instances of the explorer as it would create two
+ * ssb-server instances that conflict on the same port. Before opening explorer,
  * we check if it's already running and if it is we focus the existing window
  * rather than opening a new instance.
  */
 function quitIfAlreadyRunning () {
   if (!electron.app.requestSingleInstanceLock()) {
-    console.log('Patchwork is already running!')
+    console.log('Everlife Explorer is already running!')
     console.log('Please close the existing instance before starting a new one.')
     return electron.app.quit()
   }
@@ -236,7 +236,7 @@ function openMainWindow () {
       height: windowState.height,
       titleBarStyle: 'hiddenInset',
       autoHideMenuBar: true,
-      title: 'Patchwork',
+      title: 'Everlife Explorer',
       show: true,
       backgroundColor: '#EEE',
       icon: Path.join(__dirname, 'assets/icon.png'),
