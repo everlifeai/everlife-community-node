aeca145635b1eb17199ae365958b42f6569c9c8b
Charles L <charles.lobo@gmail.com>
2021-03-25 11:57:50 +1100

    refactor: minor cleanup

--- elife/key-management-fns.js
+++ elife/key-management-fns.js
@@ -1,126 +1,126 @@
-const { Cipher } = require("crypto");
-const fs = require("fs");
-const path = require("path");
-const u = require("@elife/utils");
-const { ipcRenderer } = require("electron");
-const { remote } = require("electron");
+const { Cipher } = require("crypto")
+const fs = require("fs")
+const path = require("path")
+const u = require("@elife/utils")
+const { ipcRenderer } = require("electron")
+const { remote } = require("electron")
 
-const Path = require("path");
-const { words } = require("lodash");
+const Path = require("path")
+const { words } = require("lodash")
 
-var pharseParam = "";
-var pharseParam2 = "";
-var phraseArry = [];
-var selectrdPhraseArr = [];
-var publickey;
-var secretkey;
-var elifeKeys;
-var secureKeys = "";
-var passMatch = false;
-var mnemonic;
-var checkboxValue = false;
-var copiedPhrase = "";
+var pharseParam = ""
+var pharseParam2 = ""
+var phraseArry = []
+var selectrdPhraseArr = []
+var publickey
+var secretkey
+var elifeKeys
+var secureKeys = ""
+var passMatch = false
+var mnemonic
+var checkboxValue = false
+var copiedPhrase = ""
 
 let img = '../../assets/img/close_icon.png'
 
 //Password Validation goes here
 function CheckPassword(inputtxt, passfld) {
   if (!inputtxt.value) {
-    document.getElementById("pass1msg").innerText = "Password is required";
-    return false;
+    document.getElementById("pass1msg").innerText = "Password is required"
+    return false
   }
 
   if (!inputtxt.value.match("^.{6,20}$")) {
-    document.getElementById("pass1msg").innerText = "Min 10 characters";
-    return false;
+    document.getElementById("pass1msg").innerText = "Min 10 characters"
+    return false
   }
 
   if (!inputtxt.value.match("(.*[a-z].*)")) {
     document.getElementById("pass1msg").innerText =
-      "Atleast one lower character";
-    return false;
+      "Atleast one lower character"
+    return false
   }
 
   if (!inputtxt.value.match("(.*[A-Z].*)")) {
     document.getElementById("pass1msg").innerText =
-      "Atleast one upper character";
-    return false;
+      "Atleast one upper character"
+    return false
   }
 
   if (!inputtxt.value.match("(.*[0-9].*)")) {
-    document.getElementById("pass1msg").innerText = "Atleast one number";
-    return false;
+    document.getElementById("pass1msg").innerText = "Atleast one number"
+    return false
   } else {
-    document.getElementById("pass1msg").innerText = "";
+    document.getElementById("pass1msg").innerText = ""
   }
   if (passfld == 2) {
     if (
       document.getElementById("newpass").value !=
       document.getElementById("confirmpass").value
     ) {
-      document.getElementById("pass2msg").innerText = "Check your password";
+      document.getElementById("pass2msg").innerText = "Check your password"
     } else {
-      document.getElementById("pass2msg").innerText = "";
-      passMatch = true;
+      document.getElementById("pass2msg").innerText = ""
+      passMatch = true
     }
   }
 }
 
 function checkBoxcheck(a) {
-  var newVal = a.value == "false" ? true : false;
+  var newVal = a.value == "false" ? true : false
   if (newVal == true)
     document.getElementById("termsOfUse").style =
-      "background: rgb(57, 30, 218)";
+      "background: rgb(57, 30, 218)"
   else
     document.getElementById("termsOfUse").style =
-      "background: rgba(210, 216, 229, 0.4);";
-  document.getElementById("termsOfUse").value = newVal;
+      "background: rgba(210, 216, 229, 0.4);"
+  document.getElementById("termsOfUse").value = newVal
 }
 
 function savePassword() {
-  window.location.href = "./step-3.html";
+  window.location.href = "./step-3.html"
   return
   if (passMatch) {
-    var CryptoJS = require("crypto-js");
-    const pass = document.getElementById("confirmpass");
-    const val = pass.value;
+    var CryptoJS = require("crypto-js")
+    const pass = document.getElementById("confirmpass")
+    const val = pass.value
 
-    var ciphertext = CryptoJS.AES.encrypt(val, "secret key 123").toString();
+    var ciphertext = CryptoJS.AES.encrypt(val, "secret key 123").toString()
     fs.writeFile(
       path.join(u.dataLoc(), "stellarPassword.txt"),
       JSON.stringify({ spw: ciphertext }),
       function (err) {
-        if (err) throw err;
-        window.location.href = "./step-3.html";
+        if (err) throw err
+        window.location.href = "./step-3.html"
       }
-    );
+    )
   } else {
-    document.getElementById("pass2msg").innerText = "Check your password";
-    return false;
+    document.getElementById("pass2msg").innerText = "Check your password"
+    return false
   }
 }
 
 function showPhrases() {
-  const StellarHDWallet = require("stellar-hd-wallet");
-  mnemonic = StellarHDWallet.generateMnemonic();
-  const wallet = StellarHDWallet.fromMnemonic(mnemonic);
+  const StellarHDWallet = require("stellar-hd-wallet")
+  mnemonic = StellarHDWallet.generateMnemonic()
+  const wallet = StellarHDWallet.fromMnemonic(mnemonic)
 
-  publickey = wallet.getPublicKey(0);
-  secretkey = wallet.getSecret(0);
-  document.getElementById("showbackup").innerHTML = mnemonic;
-  copiedPhrase = mnemonic;
-  const mnemonics = require("ssb-keys-mnemonic");
+  publickey = wallet.getPublicKey(0)
+  secretkey = wallet.getSecret(0)
+  document.getElementById("showbackup").innerHTML = mnemonic
+  copiedPhrase = mnemonic
+  const mnemonics = require("ssb-keys-mnemonic")
 
-  const words = mnemonic;
+  const words = mnemonic
 
-  elifeKeys = mnemonics.wordsToKeys(words);
+  elifeKeys = mnemonics.wordsToKeys(words)
   //Create secret file
-  const secretFile = Path.join(u.dataLoc(), "__ssb", "secret");
-  const keys = { ...elifeKeys };
-  secureKeys = keys;
-  keys.mnemonic = mnemonic;
-  keys.stellar_publickey = secretkey;
-  keys.stellar_secretkey = publickey;
+  const secretFile = Path.join(u.dataLoc(), "__ssb", "secret")
+  const keys = { ...elifeKeys }
+  secureKeys = keys
+  keys.mnemonic = mnemonic
+  keys.stellar_publickey = secretkey
+  keys.stellar_secretkey = publickey
   const lines = [
     "# this is your SECRET name.",
     "# this name gives you magical powers.",
@@ -135,13 +135,13 @@ function showPhrases() {
     "# WARNING! It's vital that you DO NOT edit OR share your secret name",
     "# instead, share your public name",
     "# your public name: " + keys.id,
-  ].join("\n");
+  ].join("\n")
   fs.writeFile(secretFile, lines, (err) => {
     if (err) {
-      console.error(err);
-      return;
+      console.error(err)
+      return
     }
-  });
+  })
 }
 
 //Downloading the mnemonic phrase into a textfile
@@ -150,15 +150,15 @@ function download() {
     if (
       document.getElementById("showphrases").innerText == "Show backup phrase"
     ) {
-      return false;
+      return false
     }
   }
   if (document.getElementById("showbackup").innerText.length > 0) {
-    var a = document.body.appendChild(document.createElement("a"));
-    a.download = "Backup_Phrase.txt";
+    var a = document.body.appendChild(document.createElement("a"))
+    a.download = "Backup_Phrase.txt"
     a.href =
-      "data:text/html," + document.getElementById("showbackup").innerHTML;
-    a.click();
+      "data:text/html," + document.getElementById("showbackup").innerHTML
+    a.click()
   }
 }
 
@@ -168,86 +168,86 @@ function copytoClipBroad() {
     if (
       document.getElementById("showphrases").innerText == "Show backup phrase"
     ) {
-      return false;
+      return false
     }
   }
   if (document.getElementById("showbackup").innerText.length > 0) {
-    var range = document.createRange();
-    range.selectNode(document.getElementById("showbackup"));
-    window.getSelection().removeAllRanges();
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-    window.getSelection().removeAllRanges();
+    var range = document.createRange()
+    range.selectNode(document.getElementById("showbackup"))
+    window.getSelection().removeAllRanges()
+    window.getSelection().addRange(range)
+    document.execCommand("copy")
+    window.getSelection().removeAllRanges()
   }
 }
 
 //on submit parse keys from step-3.html to step-4.html
 function submitBtn(){
   if(copiedPhrase.length>0){
-    window.location.href='step-4.html?phrase='+ copiedPhrase;
+    window.location.href='step-4.html?phrase='+ copiedPhrase
   }else{
     alert('Click showbackup phrase button to proceed')
   }
 }
 
 function generatekeys(){
-  const params = new URLSearchParams(window.location.search);
+  const params = new URLSearchParams(window.location.search)
   for (const param of params) {
-    mnemonic = param[1].split("^^")[0];
-    pharseParam2 = param[1].split("^^")[1];
+    mnemonic = param[1].split("^^")[0]
+    pharseParam2 = param[1].split("^^")[1]
   }
 
   //this  will shffle the array
   const shuffle = arr =>
     [...arr].reduceRight((res,_,__,s) =>
-      (res.push(s.splice(0|Math.random()*s.length,1)[0]), res),[]);
+      (res.push(s.splice(0|Math.random()*s.length,1)[0]), res),[])
 
-  phraseArry = shuffle(mnemonic.split(" "));
+  phraseArry = shuffle(mnemonic.split(" "))
   //construct mnemonic word selection  and write to DOM
-  var mnemonicHTML = "";
+  var mnemonicHTML = ""
   for (var i = 0; i < phraseArry.length; i++) {
     mnemonicHTML +=
       "<div onclick=selectedPhrase('" + phraseArry[i] +  "') class='phrase' id=phrase" + i + ">" +
       phraseArry[i] +
-      "</div>";
+      "</div>"
   }
 
-  document.getElementById("generate").innerHTML = mnemonicHTML;
+  document.getElementById("generate").innerHTML = mnemonicHTML
 }
 
 
 function selectedPhrase(inp) {
   if(selectrdPhraseArr.length==0){
     if (!selectrdPhraseArr.includes(inp)) {
-      selectrdPhraseArr.push(inp);
+      selectrdPhraseArr.push(inp)
 
-      generate(inp);
-      var imgs = document.createElement("img");
-      imgs.setAttribute("class", "phraseimg");
-      imgs.setAttribute("width", "15px");
-      imgs.setAttribute("src", img);
-      imgs.setAttribute("onclick", "imgClose('" + inp + "')");
-      document.getElementById(inp).appendChild(imgs);
+      generate(inp)
+      var imgs = document.createElement("img")
+      imgs.setAttribute("class", "phraseimg")
+      imgs.setAttribute("width", "15px")
+      imgs.setAttribute("src", img)
+      imgs.setAttribute("onclick", "imgClose('" + inp + "')")
+      document.getElementById(inp).appendChild(imgs)
     }
   }
 }
 
 function imgClose(inp) {
-  var removeEle = document.getElementById(inp);
-  var value = removeEle.getAttribute("id");
+  var removeEle = document.getElementById(inp)
+  var value = removeEle.getAttribute("id")
   selectrdPhraseArr = selectrdPhraseArr.filter(function (item) {
-    return item !== value;
-  });
-  removeEle.parentNode.removeChild(removeEle);
+    return item !== value
+  })
+  removeEle.parentNode.removeChild(removeEle)
 }
 
 function generate(inp) {
-  var objTo = document.getElementById("pharsetext");
-  var divtest = document.createElement("div");
-  divtest.setAttribute("class", "phrasetag");
-  divtest.setAttribute("id", inp);
-  divtest.innerHTML = inp;
-  objTo.appendChild(divtest);
+  var objTo = document.getElementById("pharsetext")
+  var divtest = document.createElement("div")
+  divtest.setAttribute("class", "phrasetag")
+  divtest.setAttribute("id", inp)
+  divtest.innerHTML = inp
+  objTo.appendChild(divtest)
 }
 
 //check selected phrase is 3rd index on submit
@@ -256,14 +256,14 @@ function submitPhrases(inp){
     window.location.href='step-5.html'
   }
   else{// clearing from the selected array and making the text red on wrong selection
-    selectrdPhraseArr.pop();
+    selectrdPhraseArr.pop()
     document.getElementById('err-txt').classList.add("highlight")
     document.getElementById('pharsetext').innerHTML=''
   }
 }
 
 function goBack() {
-  window.history.back();
+  window.history.back()
 }
 
 function copyPhrases(){
@@ -284,4 +284,3 @@ function termsAndConditions(){
 function privacyPolicyStellar(){
   require("electron").shell.openExternal('https://stellar.org/privacy-policy');
 }
-
